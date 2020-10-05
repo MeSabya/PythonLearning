583bd58a43bd4655a375c0f861ee2ed6f0a016e6
sabyasachi nayak <nayak.saabyasachi@gmail.com>
2020-10-05 23:41:25 +0530

    Shallow copy VS deep copy

--- ObjectOrientedIdioms/shallow_copy_vs_deep_copy.py
+++ ObjectOrientedIdioms/shallow_copy_vs_deep_copy.py
@@ -12,7 +12,6 @@ person2 = person1
 print("person2 members are: ", person2, "person1 members are: ", person1)
 print(person1 is person2) #True
 
-
 person3 = copy.copy(person1)
 print(person1 is person3) #o/p False
 person3.siblings.append("Mantu")
@@ -24,6 +23,7 @@ print("person4 members are: ", person4, "person1 members are: ", person1)
 
 # Example1:
 #######################################################
+print("Example1")
 l1 = [1, 2, 3]
 l2 = list(l1)
 
@@ -33,6 +33,7 @@ print("Print l2", l2)
 
 # Example 2
 ##########################################################
+print("Example2")
 l3 = [[1, 2, 3], [4, 5, 6]]
 l4 = list(l3)
 
@@ -42,6 +43,7 @@ print("Print l2", l2)
 
 #Example 3
 ###########################################################
+print("Example3")
 l5 = [[1, 'a'], [2, 'b'], [3, 'c']]
 l6 = list(l5)
 
@@ -49,3 +51,18 @@ l5.append([4, 'd'])
 print(l5)
 print(l6)
 
+'''
+Output:
+
+person2 members are:  Raj simran chintu pintu person1 members are:  Raj simran chintu pintu
+True
+False
+person3 members are:  Raj simran chintu pintu Mantu person1 members are:  Raj simran chintu pintu Mantu
+person4 members are:  Raj simran chintu pintu Mantu Hunnn person1 members are:  Raj simran chintu pintu Mantu
+print l1 [4, 2, 3]
+Print l2 [1, 2, 3]
+print l1 [4, 2, 3]
+Print l2 [1, 2, 3]
+[[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']]
+[[1, 'a'], [2, 'b'], [3, 'c']]
+'''
